30ffdaa87ddd494bf7756edaa33261a805ae5c77
nathan <nathanmageka6@gmail.com>
2023-03-18 23:30:11 +0300

    nathan

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31
+const num2 =2
+const multiply=(num1*num2)
 
-// Write your code here
+let num3 = 29
+let num4= 5
+let mod = (num3%= num4)
+
+const random = 5
+
+let max = 20
+max_math(0,20)
